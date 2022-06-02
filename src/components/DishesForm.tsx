199df859aa7c4ab5f813260bc74efa81c922f9cc
adriano9631 <adumik123@gmail.com>
2022-06-02 18:51:50 +0200

    add error handling and feedback

--- src/components/DishesForm.tsx
+++ src/components/DishesForm.tsx
@@ -13,7 +13,9 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Snackbar,
 } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 import axiosInstance from "../api/axios-instance";
 import styles from "./DishesForm.module.scss";
@@ -32,7 +34,18 @@ const DishesForm: FC = () => {
 
   const [datePickerValue, setDatePickerValue] = useState<Date | null>(null);
   const [preparation_time, setPreparation_time] = useState<string | null>(null);
-  const { control, handleSubmit, setValue } = useForm<DishesFormTypes>();
+  const { control, handleSubmit, setValue, formState, reset } =
+    useForm<DishesFormTypes>({
+      mode: "onChange",
+      defaultValues: {
+        name: "",
+        dish_type: "",
+        no_of_slices: 1,
+        diameter: "",
+        spiciness_scale: 1,
+        slices_of_bread: 1,
+      },
+    });
 
   useEffect(() => {
     const testData = {
@@ -73,6 +86,9 @@ const DishesForm: FC = () => {
     };
 
     await axiosInstance.post("dishes", dishDetails);
+    setIsOpenSnackbar(true);
+    reset();
+    setDatePickerValue(null);
   };
 
   const dish_type = useWatch({
@@ -96,145 +112,185 @@ const DishesForm: FC = () => {
     setValue("diameter", Number(diameter).toFixed(2));
   };
 
-  return (
-    <form className={styles.dishesForm} onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="name"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField required label="Dish name" variant="outlined" {...field} />
-        )}
-      />
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <TimePicker
-          ampm={false}
-          openTo="hours"
-          views={["hours", "minutes", "seconds"]}
-          inputFormat="HH:mm:ss"
-          mask="__:__:__"
-          label="Preparation time"
-          value={datePickerValue}
-          onChange={handleTimePickerChange}
-          renderInput={(params) => <TextField required {...params} />}
-        />
-      </LocalizationProvider>
+  const [isOpenSnackbar, setIsOpenSnackbar] = useState(false);
 
-      <FormControl fullWidth variant="filled">
-        <InputLabel id="dish_typeId-label">Choose dish</InputLabel>
+  const handleCloseSnackbar = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setIsOpenSnackbar(false);
+  };
+
+  return (
+    <div>
+      <form className={styles.dishesForm} onSubmit={handleSubmit(onSubmit)}>
         <Controller
-          render={({ field }) => (
-            <Select
-              required
-              {...field}
-              labelId="dish_typeId-label"
-              id="dish_typeId"
-            >
-              <MenuItem value="pizza">Pizza</MenuItem>
-              <MenuItem value="soup">Soup</MenuItem>
-              <MenuItem value="sandwich">Sandwich</MenuItem>
-            </Select>
-          )}
-          name="dish_type"
+          name="name"
           control={control}
           defaultValue=""
+          render={({ field }) => (
+            <TextField
+              required
+              label="Dish name"
+              variant="outlined"
+              {...field}
+            />
+          )}
         />
-      </FormControl>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <TimePicker
+            ampm={false}
+            openTo="hours"
+            views={["hours", "minutes", "seconds"]}
+            inputFormat="HH:mm:ss"
+            mask="__:__:__"
+            label="Preparation time"
+            value={datePickerValue}
+            onChange={handleTimePickerChange}
+            renderInput={(params) => <TextField required {...params} />}
+          />
+        </LocalizationProvider>
 
-      {dish_type === "pizza" && (
-        <>
+        <FormControl fullWidth variant="filled">
+          <InputLabel id="dish_typeId-label">Choose dish</InputLabel>
           <Controller
-            name="no_of_slices"
+            render={({ field }) => (
+              <Select
+                required
+                {...field}
+                labelId="dish_typeId-label"
+                id="dish_typeId"
+              >
+                <MenuItem value="pizza">Pizza</MenuItem>
+                <MenuItem value="soup">Soup</MenuItem>
+                <MenuItem value="sandwich">Sandwich</MenuItem>
+              </Select>
+            )}
+            name="dish_type"
+            control={control}
+            defaultValue=""
+          />
+        </FormControl>
+
+        {dish_type === "pizza" && (
+          <>
+            <Controller
+              name="no_of_slices"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  required
+                  id="no_of_slices"
+                  label="The number of pizza slices"
+                  type="number"
+                  {...field}
+                  InputProps={{
+                    inputProps: {
+                      min: 0,
+                    },
+                  }}
+                />
+              )}
+            />
+            <Controller
+              name="diameter"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  required
+                  id="diameter"
+                  label="Pizza size"
+                  type="number"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">cm</InputAdornment>
+                    ),
+                    inputProps: {
+                      min: 0,
+                      step: 1,
+                    },
+                  }}
+                  {...field}
+                  onChange={(event) =>
+                    handleDiameterFormatChange(event.target.value)
+                  }
+                  value={diameter}
+                />
+              )}
+            />
+          </>
+        )}
+
+        {dish_type === "soup" && (
+          <Controller
+            name="spiciness_scale"
             control={control}
             render={({ field }) => (
               <TextField
                 required
-                id="no_of_slices"
-                label="The number of pizza slices"
+                id="spiciness_scale"
                 type="number"
+                label="Spiciness scale (1-10)"
                 {...field}
                 InputProps={{
                   inputProps: {
-                    min: 0,
+                    min: 1,
+                    max: 10,
                   },
                 }}
               />
             )}
           />
+        )}
+
+        {dish_type === "sandwich" && (
           <Controller
-            name="diameter"
+            name="slices_of_bread"
             control={control}
             render={({ field }) => (
               <TextField
                 required
-                id="diameter"
-                label="Pizza size"
                 type="number"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">cm</InputAdornment>
-                  ),
-                  inputProps: {
-                    min: 0,
-                    step: 1,
-                  },
-                }}
+                defaultValue=""
+                id="slices_of_bread"
+                label="Slices of bread"
                 {...field}
-                onChange={(event) =>
-                  handleDiameterFormatChange(event.target.value)
-                }
-                value={diameter}
               />
             )}
           />
-        </>
-      )}
+        )}
+        
+        {serviceUnavailableError && (
+          <Alert severity="error">{serviceUnavailableError}</Alert>
+        )}
 
-      {dish_type === "soup" && (
-        <Controller
-          name="spiciness_scale"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              required
-              id="spiciness_scale"
-              type="number"
-              label="Spiciness scale (1-10)"
-              {...field}
-              InputProps={{
-                inputProps: {
-                  min: 1,
-                  max: 10,
-                },
-              }}
-            />
-          )}
-        />
-      )}
+        <Button variant="contained" type="submit">
+          Save
+        </Button>
+      </form>
 
-      {dish_type === "sandwich" && (
-        <Controller
-          name="slices_of_bread"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              required
-              type="number"
-              defaultValue=""
-              id="slices_of_bread"
-              label="Slices of bread"
-              {...field}
-            />
-          )}
-        />
+      {formState.isSubmitSuccessful && (
+        <Snackbar
+          open={isOpenSnackbar}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+        >
+          <MuiAlert
+            onClose={handleCloseSnackbar}
+            severity="success"
+            sx={{ width: "100%" }}
+            elevation={6}
+            variant="filled"
+          >
+            Recipe has been successfully saved!
+          </MuiAlert>
+        </Snackbar>
       )}
-      {serviceUnavailableError && (
-        <Alert severity="error">{serviceUnavailableError}</Alert>
-      )}
-      <Button variant="contained" type="submit">
-        Save
-      </Button>
-    </form>
+    </div>
   );
 };
 
